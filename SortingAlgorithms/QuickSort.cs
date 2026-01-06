528b931469445e2de87364d3dc0095488419ddd2
sohil-khann <khaa40408@gmail.com>
2026-01-06 15:47:55 +0530

    [Sohil khan] Add. quick sort

---
+++ SortingAlgorithms/QuickSort.cs
@@ -0,0 +1,11 @@
+/*4. Quick Sort - Sort Product Prices
+Problem Statement:
+An e-commerce company wants to display product prices in ascending order. Implement
+Quick Sort in C# to sort the product prices.
+Hint:
+ Pick a pivot element (first, last, or random).
+ Partition the array such that elements smaller than the pivot are on the left and
+larger ones are on the right.
+ Recursively apply Quick Sort on left and right partitions.*/
+
+using System;internal static class QuickSort{    internal static void Call()    {        int[] productPrices = { 499, 299, 999, 199, 799, 399, 599,-50 };        quickSort(productPrices, 0, productPrices.Length - 1);        int n = productPrices.Length;        for (int i = 0; i < n; ++i)            Console.Write(productPrices[i] + " ");        Console.WriteLine();    }    // This function takes last element as pivot,    // places the pivot element at its correct    // position in sorted array, and places all    // smaller (smaller than pivot) to left of    // pivot and all greater elements to right    // of pivot    static int partition(int[] arr, int low, int high)    {        int pivot = arr[high]; // pivot        int i = (low - 1); // Index of smaller element        for (int j = low; j < high; j++)        {            // If current element is smaller            // than or equal to pivot            if (arr[j] <= pivot)            {                i++;                // swap arr[i] and arr[j]                int temp = arr[i];                arr[i] = arr[j];                arr[j] = temp;            }        }        // swap arr[i+1] and arr[high]         int temp1 = arr[i + 1];        arr[i + 1] = arr[high];        arr[high] = temp1;        return i + 1;    }    // The main function that implements QuickSort    // arr[] = Array to be sorted,    // low = Starting index,    // high = Ending index    static void quickSort(int[] arr, int low, int high)    {        if (low < high)        {            // pi is partitioning index, arr[pi] is            // now at right place            int pi = partition(arr, low, high);            // Recursively sort elements before            // partition and after partition            quickSort(arr, low, pi - 1);            quickSort(arr, pi + 1, high);        }    }}

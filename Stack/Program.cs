8d3cab1a00244c300b27bc4fa7bfafaf0a8e129d
sohil-khann <khaa40408@gmail.com>
2026-01-09 15:41:45 +0530

    [Sohil khan] Add. Program.cs

---
+++ Stack/Program.cs
@@ -0,0 +1,29 @@
+﻿   
+using System;
+namespace Stack{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for expression evaluation
+
+            // ExpressionEvaluation eval = new ExpressionEvaluation();
+            // eval.Call();
+
+            //for tab navigation
+            // TabNavigationMain tabNav = new TabNavigationMain();
+            // tabNav.Call();
+
+            //for valid paranthesis
+            // ValidParanthesis vp = new ValidParanthesis();
+            // vp.Call();
+
+            //for undo functionality
+            UndoFunc undoDemo = new UndoFunc();
+            undoDemo.Call();
+
+            Console.ReadLine();
+        }
+    }
+}
+
